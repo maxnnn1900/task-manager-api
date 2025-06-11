ba11e12f8ca43c00eaff7544853638dc7df1e96c
maxnnn1900 <maxnnn1900@gmail.com>
2025-06-11 18:54:09 +0300

    task#2 filter tasks

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,12 +6,42 @@ use App\Enums\TaskStatus;
 use App\Models\Task;
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
+use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return Task::all();
+        $query = Task::query();
+
+        if ($request->filled('status')) {
+            $query->where('status', $request->input('status'));
+        }
+
+        if ($request->filled('created_from')) {
+            $query->whereDate('created_at', '>=', $request->input('created_from'));
+        }
+
+        if ($request->filled('created_to')) {
+            $query->whereDate('created_at', '<=', $request->input('created_to'));
+        }
+
+        if ($request->filled('employee_id')) {
+            $query->whereHas('employees', function ($q) use ($request) {
+                $q->where('employees.id', $request->input('employee_id'));
+            });
+        }
+
+        $sortable = ['title', 'created_at', 'status'];
+        $sortBy = in_array($request->input('sort_by'), $sortable)
+            ? $request->input('sort_by')
+            : 'created_at';
+
+        $sortDir = $request->input('sort_dir') === 'asc' ? 'asc' : 'desc';
+
+        $query->orderBy($sortBy, $sortDir);
+
+        return $query->paginate(20);
     }
 
     public function store(StoreTaskRequest $request)
